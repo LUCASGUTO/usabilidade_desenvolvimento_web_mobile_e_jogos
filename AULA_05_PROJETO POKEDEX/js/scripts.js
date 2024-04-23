c7b69bb11eb26ee57495c8e6d1eb7b68301fc173
LUCASGUTO <137459857+LUCASGUTO@users.noreply.github.com>
2024-04-22 21:06:26 -0300

    ajuste

--- AULA_05_PROJETO POKEDEX/js/scripts.js
+++ AULA_05_PROJETO POKEDEX/js/scripts.js
@@ -1,72 +1,48 @@
 
-const grupoGrass=["bulbasaur","ivysaur","venusaur","pidgey","pidgeotto","pidgeot"]
-const grupoFire=["charmander","charmeleon","charizard"]
-const grupoWater=["squirtle",'wartortle','blastoise']
-const grupoEarth=["caterpie","metapod","butterfree","weedle","kakuna","beedrill","rattata","raticate"]
+const grupoGrama = ["bulbasaur","venusaur","ivysaur"]
+const grupoFogo = ["charmander","charmeleon","charizard"]
 
 
 function carregarPokemons(){
- 
 
-    const requisicaoHTTPP = new XMLHttpRequest()
-
-    requisicaoHTTPP.open("GET","https://pokeapi.co/api/v2/pokemon",false)
-    requisicaoHTTPP.send()
-
-    const resposta=JSON.parse(requisicaoHTTPP.responseText)
+    const requisicaoHttp = new XMLHttpRequest()
+    requisicaoHttp.open("GET","https://pokeapi.co/api/v2/pokemon",false)
+    requisicaoHttp.send()
     
-    const pokemons = resposta.results   
+    const resposta = JSON.parse(requisicaoHttp.responseText)
+    const pokemons = resposta.results
 
-    
-    for(let index = 0; index < pokemons.length; index++){
+    for( let index = 0 ; index < pokemons.length; index ++){
         const pokemon = pokemons[index]
-
         const id = index + 1
-
-        pokemon.imagem="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/"+ id +".png"
-
-        addCardPokemon(pokemon)
+        pokemon.imagem = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + id +".png"
+        adicionarCardPokemon(pokemon)
     }
-
-    
-
 }
 
-function addCardPokemon(pokemon){
+function adicionarCardPokemon(pokemon){
 
-    
-    console.log(pokemon)
+    const divCardElemento = document.createElement("div")
+    divCardElemento.classList.add("card")
 
     const imagemElemento = document.createElement("img")
-
     imagemElemento.setAttribute("src", pokemon.imagem)
+    imagemElemento.style.width="90%"
 
+    const textoElemento = document.createElement("h1")
+    textoElemento.innerHTML = pokemon.name
 
+    if(grupoGrama.includes(pokemon.name)){
+        textoElemento.classList.add("grama")
+    } else if(grupoFogo.includes(pokemon.name)){
+        textoElemento.classList.add("fogo")
+    }
+   
     const divElemento = document.getElementById("conteudo-pokemon")
-    divElemento.classList.add("card")
-    divElemento.appendChild(imagemElemento)
-    
 
-    const nomeElemento = document.createElement("p")
+    divCardElemento.appendChild(imagemElemento)
+    divCardElemento.appendChild(textoElemento)
 
-    imagemElemento.setAttribute("p", pokemon.name)
-    
-    nomeElemento.textContent=pokemon.name;
-
-    if(grupoGrass.includes(pokemon.name)){
-        nomeElemento.classList.add("grass")
-   }else if(grupoFire.includes(pokemon.name)){
-        nomeElemento.classList.add("fire")
-
-   }else if(grupoWater.includes(pokemon.name)){
-        nomeElemento.classList.add("water")
-
-   }else if(grupoEarth.includes(pokemon.name)){
-    nomeElemento.classList.add("earth")
-   }
-
-    divElemento.appendChild(nomeElemento)
-
+    divElemento.appendChild(divCardElemento)
 
 }
-
