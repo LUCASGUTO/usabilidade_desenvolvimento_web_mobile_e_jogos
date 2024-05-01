0aaa10bb5105aaae17dd8514a285497328ee98e2
LUCASGUTO <137459857+LUCASGUTO@users.noreply.github.com>
2024-04-30 21:30:39 -0300

    ajuste_final

--- Aula_09/js/scripts.js
+++ Aula_09/js/scripts.js
@@ -3,6 +3,20 @@ function onPageLoad(){
 
     fetch("https://randomuser.me/api/?results=50")
     .then(function response(data){
+
+        const promiseData= data.json()
+        console.log(promiseData)
+        
+        promiseData.then(response=>{
+        
+            insertUsers(response.results)
+
+         })
+        
+   /* promiseData.then(function responseData(response){
+        insertUsers(response)
+    })
+    */
         insertUsers(data)
     })
 
