fe401de467b4a70c6d1808dab14684d5a7706c74
LUCASGUTO <137459857+LUCASGUTO@users.noreply.github.com>
2024-05-07 20:46:33 -0300

    ajuste_pokedex

--- AULA_05_PROJETO POKEDEX/js/scripts.js
+++ AULA_05_PROJETO POKEDEX/js/scripts.js
@@ -5,20 +5,63 @@ const grupoFogo = ["charmander","charmeleon","charizard"]
 
 function carregarPokemons(){
 
-    const requisicaoHttp = new XMLHttpRequest()
-    requisicaoHttp.open("GET","https://pokeapi.co/api/v2/pokemon",false)
-    requisicaoHttp.send()
-    
-    const resposta = JSON.parse(requisicaoHttp.responseText)
-    const pokemons = resposta.results
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=1000")
+    .then(function response(data){
 
-    for( let index = 0 ; index < pokemons.length; index ++){
-        const pokemon = pokemons[index]
-        const id = index + 1
-        pokemon.imagem = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + id +".png"
-        adicionarCardPokemon(pokemon)
-    }
+        const promiseData= data.json()
+        console.log(promiseData)
+        
+        promiseData.then(response=>{
+        
+            insertPokemon(response.results)
+            const pokemons = response.results
+
+            for( let index = 0 ; index < pokemons.length; index ++){
+                const pokemon = pokemons[index]
+                const id = index + 1
+                pokemon.imagem = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + id +".png"
+                adicionarCardPokemon(pokemon)
+            }
+            
+         })
+        
+   /* promiseData.then(function responseData(response){
+        insertUsers(response)
+    })
+    */
+        insertPokemon(data)
+    })
+
+    loadComponents()
 }
+
+function insertPokemon (data){
+    console.log(data)
+}
+
+function loadComponents(){
+
+    console.log("loadComponents")
+
+
+}
+
+
+    
+    // const requisicaoHttp = new XMLHttpRequest()
+    // requisicaoHttp.open("GET","https://pokeapi.co/api/v2/pokemon",false)
+    // requisicaoHttp.send()
+    
+    // const resposta = JSON.parse(requisicaoHttp.responseText)
+    // const pokemons = resposta.results
+
+//     for( let index = 0 ; index < pokemons.length; index ++){
+//         const pokemon = pokemons[index]
+//         const id = index + 1
+//         pokemon.imagem = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + id +".png"
+//         adicionarCardPokemon(pokemon)
+//     }
+// }
 
 function adicionarCardPokemon(pokemon){
 
